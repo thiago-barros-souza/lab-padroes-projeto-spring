ec622b43bb85d7677979efced1debfe0d8bf2820
Thiago Barros <thiagob068@gmail.com>
2022-11-22 11:07:50 -0300

    Remoção de comentários

--- src/main/java/one/digitalinnovation/gof/model/Cliente.java
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -20,28 +20,4 @@ public class Cliente {
 	
 	@ManyToOne
 	private Endereco endereco;
-
-//	public Long getId() {
-//		return id;
-//	}
-//
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//
-//	public String getNome() {
-//		return nome;
-//	}
-//
-//	public void setNome(String nome) {
-//		this.nome = nome;
-//	}
-//
-//	public Endereco getEndereco() {
-//		return endereco;
-//	}
-//
-//	public void setEndereco(Endereco endereco) {
-//		this.endereco = endereco;
-//	}
 }
